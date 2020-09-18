372b1db05aa43d64a4210dad5f25e9ce2f59cfab
Xuan Tang <txstc55@gmail.com>
2020-09-18 17:50:47 -0500

    remove script

--- .github/run-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd ../interval_operations/src && ./compile.sh 
-cd ../../
-rm -rf build && mkdir build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make
-./unit_tests >> test_results.txt
-grep -Ri "fail" test_results.txt
