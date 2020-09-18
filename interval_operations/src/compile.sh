b43622d38cd3aa6afd4b9abaf5cfc2dc4bfe623f
Xuan Tang <txstc55@gmail.com>
2020-09-18 17:11:21 -0500

    ready for action

--- interval_operations/src/compile.sh
+++ interval_operations/src/compile.sh
@@ -6,5 +6,9 @@ echo "Compile to object file"
 /usr/local/opt/llvm/bin/llc -filetype=obj interval.bc -o interval.o
 echo "Compile to dynamic library"
 clang interval.o -o interval.dylib -dynamiclib -shared
-echo "Found operatiuons"
+echo "Compile to shared object"
+clang interval.o -o interval.so -shared
+echo "Found operations of dylib"
 nm -gU interval.dylib
+echo "Found operations of so"
+nm -gD interval.so
