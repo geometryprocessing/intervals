89b69eb835d879ba21b8302156ba2bc7b4cf40df
Xuan Tang <txstc55@gmail.com>
2020-09-19 14:37:48 -0500

    compile differently

--- interval_operations/src/compile.sh
+++ interval_operations/src/compile.sh
@@ -1,13 +1,5 @@
 # echo "Compile to llvm ir"
 # gcc++ -fpic interval.cc -emit-llvm -S -c -o interval.ll -O1
-echo "Compile to bitcode"
-/usr/local/opt/llvm/bin/llvm-as interval.ll -o interval.bc
-echo "Compile to object file"
-/usr/local/opt/llvm/bin/llc -filetype=obj interval.bc -o interval.o
-echo "Compile to dynamic library"
-gcc interval.o -o interval.dylib -dynamiclib -shared
-echo "Compile to shared object"
-gcc interval.o -o interval.so -shared
 
 unameOut="$(uname -s)"
 case "${unameOut}" in
@@ -20,9 +12,25 @@ esac
 echo ${machine}
 
 if [ "${machine}" == "Mac" ]; then
+    echo "Compile to bitcode"
+    /usr/local/opt/llvm/bin/llvm-as interval.ll -o interval.bc
+    echo "Compile to object file"
+    /usr/local/opt/llvm/bin/llc -filetype=obj interval.bc -o interval.o
+    echo "Compile to dynamic library"
+    gcc interval.o -o interval.dylib -dynamiclib -shared
+    echo "Compile to shared object"
+    gcc interval.o -o interval.so -shared
     echo "Found operations of dylib"
     nm -gU interval.dylib
 elif [ "${machine}" == "Linux" ]; then
+    echo "Compile to bitcode"
+    llvm-as interval.ll -o interval.bc
+    echo "Compile to object file"
+    llc -filetype=obj interval.bc -o interval.o
+    echo "Compile to dynamic library"
+    gcc interval.o -o interval.dylib -dynamiclib -shared
+    echo "Compile to shared object"
+    gcc interval.o -o interval.so -shared
     echo "Found operations of so"
     nm -gD interval.so
 fi
