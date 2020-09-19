bcfa1894a9d09ed096e6b6f53bfa7f9523a6f76b
Xuan Tang <txstc55@gmail.com>
2020-09-19 14:12:00 -0500

    use gcc

--- interval_operations/src/compile.sh
+++ interval_operations/src/compile.sh
@@ -1,13 +1,13 @@
 # echo "Compile to llvm ir"
-# clang++ -fpic interval.cc -emit-llvm -S -c -o interval.ll -O1
+# gcc++ -fpic interval.cc -emit-llvm -S -c -o interval.ll -O1
 echo "Compile to bitcode"
 /usr/local/opt/llvm/bin/llvm-as interval.ll -o interval.bc
 echo "Compile to object file"
 /usr/local/opt/llvm/bin/llc -filetype=obj interval.bc -o interval.o
 echo "Compile to dynamic library"
-clang interval.o -o interval.dylib -dynamiclib -shared
+gcc interval.o -o interval.dylib -dynamiclib -shared
 echo "Compile to shared object"
-clang interval.o -o interval.so -shared
+gcc interval.o -o interval.so -shared
 
 unameOut="$(uname -s)"
 case "${unameOut}" in
