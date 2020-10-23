eb4de6914bd3c3d4585f9de8642ffb955f1368e1
Xuan Tang <txstc55@gmail.com>
2020-10-22 23:48:57 -0500

    do not use c++ version of filib

--- tests/flags.hpp
+++ tests/flags.hpp
@@ -1,4 +1,4 @@
-#define USE_FILIB_PLUSPLUS // weather we will use the filib plus plus version
+// #define USE_FILIB_PLUSPLUS // weather we will use the filib plus plus version
 // #define USE_SYSTEM_RANDOM  // weather we will use the system random or a redefined set of random numbers
 
 #ifdef USE_FILIB_PLUSPLUS
