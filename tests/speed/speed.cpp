31d481cc5712c6056cf838e9a93a7cd4ac966516
Xuan Tang <txstc55@gmail.com>
2020-10-29 11:11:22 -0500

    switch the variable position

--- tests/speed/speed.cpp
+++ tests/speed/speed.cpp
@@ -9,7 +9,7 @@ double boost_timer, fic_timer, native_switch_timer, multiplicative_timer, pred_s
 // do whatever you want for the spped thest
 // see the helper_functions.hpp for input and output deatail
 #ifdef USE_FILIB_PLUSPLUS
-#define RUN_SPEED(METHOD, VARIABLE_COUNT, INFO, DISTRIBUTION)                                                                                                   \
+#define RUN_SPEED(METHOD, INFO, VARIABLE_COUNT, DISTRIBUTION)                                                                                                   \
     {                                                                                                                                                           \
         boost_timer = 0;                                                                                                                                        \
         fic_timer = 0;                                                                                                                                          \
@@ -36,39 +36,39 @@ double boost_timer, fic_timer, native_switch_timer, multiplicative_timer, pred_s
     }                                                                                                                                                           \
     while (0)
 #else
-#define RUN_SPEED(METHOD, VARIABLE_COUNT, INFO, DISTRIBUTION)                                                                                                   \
-    {                                                                                                                                                           \
-        boost_timer = 0;                                                                                                                                        \
-        fic_timer = 0;                                                                                                                                          \
-        native_switch_timer = 0;                                                                                                                                \
-        multiplicative_timer = 0;                                                                                                                               \
-        pred_succ_timer = 0;                                                                                                                                    \
-        for (int i = 0; i < SPEED_TEST_LOOP; i++)                                                                                                               \
-        {                                                                                                                                                       \
-            PRE_FILL_RANDOM_DOUBLES(DISTRIBUTION, VARIABLE_COUNT);                                                                                              \
-            COMPUTE_TIME(fic_interval, METHOD, VARIABLE_COUNT, fic_timer);                                                                                      \
-            COMPUTE_TIME(boost_interval, METHOD, VARIABLE_COUNT, boost_timer);                                                                                  \
-        }                                                                                                                                                       \
-        printf("TIME, %s, FILIB C, %lf us, %lfx time that FILIB C version took\n", INFO, fic_timer, fic_timer / fic_timer);                                     \
-        printf("TIME, %s, BOOST, %lf us, %lfx time that FILIB C version took\n", INFO, boost_timer, boost_timer / fic_timer);                                   \
-    }                                                                                                                                                           \
+#define RUN_SPEED(METHOD, INFO, VARIABLE_COUNT, DISTRIBUTION)                                                                 \
+    {                                                                                                                         \
+        boost_timer = 0;                                                                                                      \
+        fic_timer = 0;                                                                                                        \
+        native_switch_timer = 0;                                                                                              \
+        multiplicative_timer = 0;                                                                                             \
+        pred_succ_timer = 0;                                                                                                  \
+        for (int i = 0; i < SPEED_TEST_LOOP; i++)                                                                             \
+        {                                                                                                                     \
+            PRE_FILL_RANDOM_DOUBLES(DISTRIBUTION, VARIABLE_COUNT);                                                            \
+            COMPUTE_TIME(fic_interval, METHOD, VARIABLE_COUNT, fic_timer);                                                    \
+            COMPUTE_TIME(boost_interval, METHOD, VARIABLE_COUNT, boost_timer);                                                \
+        }                                                                                                                     \
+        printf("TIME, %s, FILIB C, %lf us, %lfx time that FILIB C version took\n", INFO, fic_timer, fic_timer / fic_timer);   \
+        printf("TIME, %s, BOOST, %lf us, %lfx time that FILIB C version took\n", INFO, boost_timer, boost_timer / fic_timer); \
+    }                                                                                                                         \
     while (0)
 #endif
 
 int main(int argc, char *argv[])
 {
     // we will run each method defined in method.hpp for each available interval library
-    RUN_SPEED(addition, 2, "ADDITION", distribution_all_range);
-    RUN_SPEED(subtraction, 2, "SUBTRACTION", distribution_all_range);
-    RUN_SPEED(multiplication, 2, "MULTIPLICATION", distribution_all_range);
-    RUN_SPEED(division, 2, "DIVISION", distribution_all_range);
-    RUN_SPEED(expr1, 10, "EXPRESSION 1", distribution_all_range);
-    RUN_SPEED(expr2, 10, "EXPRESSION 2", distribution_all_range);
-    RUN_SPEED(expr3, 10, "EXPRESSION 3", distribution_all_range);
-    RUN_SPEED(square_root, 1, "SQUARE ROOT", distribution_positive);
-    RUN_SPEED(exponential, 1, "EXPONENTIAL", distribution_exp);
-    RUN_SPEED(sin, 1, "SIN", distribution_all_range);
-    RUN_SPEED(cos, 1, "COS", distribution_all_range);
+    RUN_SPEED(addition, "ADDITION", 2, distribution_all_range);
+    RUN_SPEED(subtraction, "SUBTRACTION", 2, distribution_all_range);
+    RUN_SPEED(multiplication, "MULTIPLICATION", 2, distribution_all_range);
+    RUN_SPEED(division, "DIVISION", 2, distribution_all_range);
+    RUN_SPEED(expr1, "EXPRESSION 1", 10, distribution_all_range);
+    RUN_SPEED(expr2, "EXPRESSION 2", 10, distribution_all_range);
+    RUN_SPEED(expr3, "EXPRESSION 3", 10, distribution_all_range);
+    RUN_SPEED(square_root, "SQUARE ROOT", 1, distribution_positive);
+    RUN_SPEED(exponential, "EXPONENTIAL", 1, distribution_exp);
+    RUN_SPEED(sin, "SIN", 1, distribution_all_range);
+    RUN_SPEED(cos, "COS", 1, distribution_all_range);
 
     return 0;
 }
