0425801a881b2655c0cbeffb825564966d1b0e14
Xuan Tang <txstc55@gmail.com>
2020-10-30 16:08:05 -0500

    ad pragma once

--- src/flags.hpp
+++ src/flags.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #define USE_SYSTEM_RANDOM     // weather we will use the system random or a redefined set of random numbers
 #define SPEED_TEST_SIZE 1000  // how many different set of value we do for speed test
 #define SPEED_TEST_LOOP 10000 // for each set of value, how many loops
